e24b92381beff576ccc20ec1ac442d03c5040b5b
bringebass <65865260+bringebass@users.noreply.github.com>
2020-06-15 23:59:55 +0200

    vvdasfdsavvxcz
    vcxzvcxzvccxzs

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function myFunction() {
-    document.getElementById("nav-boxy").classList.toggle("show");
-  }
+  // Open nav bar when hamburger menu is clicked
+  document.getElementById('hambruger-menu').addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.getElementById('nav-boxy').classList.toggle('show');
+  });
+  
+  // close hamburger menu if body is clicked ONLY when the menu is already visible
+  document.body.addEventListener('click', () => {
+    const navBoxEl = document.getElementById("nav-boxy")
+    if(navBoxEl.className === 'nav-boxy-class show') {
+      navBoxEl.classList.toggle('show');
+    }
+  });
