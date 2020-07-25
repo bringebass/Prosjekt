1ae1014eb96ec876ae7b7860f46782ad50adefcd
Oyvind Bringedal <oyvindbringedal@Oyvinds-MacBook-Pro.local>
2020-07-25 18:30:39 +0200

    bklhjjbkl

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-  // Open nav bar when hamburger menu is clicked
-  document.getElementById('hambruger-menu').addEventListener('click', (e) => {
-    e.stopPropagation();
-    document.getElementById('nav-boxy').classList.toggle('show');
-  });
-  
-  // close hamburger menu if body is clicked ONLY when the menu is already visible
-  document.body.addEventListener('click', () => {
-    const navBoxEl = document.getElementById("nav-boxy")
-    if(navBoxEl.className === 'nav-boxy-class show') {
-      navBoxEl.classList.toggle('show');
-    }
-  });
 
+
+// Open nav bar when hamburger menu is clicked
+document.getElementById('hambruger-menu').addEventListener('click', (e) => {
+e.stopPropagation();
+document.getElementById('nav-boxy').classList.toggle('show');
+});
   
-  function myFunction() {
-    var element = document.getElementById("rullegardin-id");
-    element.classList.toggle("rullegardin-show");
-  }
+// close hamburger menu if body is clicked ONLY when the menu is already visible
+document.body.addEventListener('click', () => {
+const navBoxEl = document.getElementById("nav-boxy")
+if(navBoxEl.className === 'nav-boxy-class show') {
+navBoxEl.classList.toggle('show');
+}
+});
+
+const vinkel = document.getElementById('vinkel');
+const content = document.querySelector('.content', '#rullegardin-id');
+const rullegardin = document.getElementById('rullegardin-id');
+const cfv = document.querySelector('.cfv');
+
+
+
+cfv.addEventListener('click', () => {
+vinkel.classList.toggle('rotate'),
+rullegardin.classList.toggle('rullegardin-show');
+});
 
 
 
