e42f5db7e4a44a30d1808d959be57c4f0d7c647a
Oyvind Bringedal <oyvindbringedal@Oyvinds-MacBook-Pro.local>
2020-07-26 13:58:28 +0200

    gfgfdgsfds

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-
+//const vinkel = document.getElementById('vinkel');
+//const vinkelTopp = document.getElementById('vink-topp');
+//const content = document.querySelector('.content', '#rullegardin-id');
+//const rullegardin = document.getElementById('rullegardin-id');
+//const cfv = document.querySelector('.cfv');
+const dropdownContent = document.querySelector('.dropdown-content');
+const reviews = document.querySelector('.parent');
 
 // Open nav bar when hamburger menu is clicked
 document.getElementById('hambruger-menu').addEventListener('click', (e) => {
@@ -15,29 +21,22 @@ navBoxEl.classList.toggle('show');
 
 });
 
-const vinkel = document.getElementById('vinkel');
-const vinkelTopp = document.getElementById('vink-topp');
-const content = document.querySelector('.content', '#rullegardin-id');
-const rullegardin = document.getElementById('rullegardin-id');
-const cfv = document.querySelector('.cfv');
-const dropdownContent = document.querySelector('.dropdown-content');
-const reviews = document.querySelector('.parent');
-
-
-cfv.addEventListener('click', () => {
-vinkel.classList.toggle('rotate'),
-rullegardin.classList.toggle('rullegardin-show');
-});
-
 reviews.addEventListener('click', (e) => {
-e.stopPropagation();
-vinkelTopp.classList.toggle('rotate'),
-dropdownContent.classList.toggle('show-dropdown');
+  e.stopPropagation();
+  document.getElementById('vink-topp').classList.toggle('rotate'),
+  dropdownContent.classList.toggle('show-dropdown');
+  });
+  
+  
+  document.body.addEventListener('click', () => {
+   if (dropdownContent.className === 'dropdown-content show-dropdown') {
+    dropdownContent.classList.toggle('show-dropdown');
+  }
+   });
+
+
+document.querySelector('.cfv').addEventListener('click', () => {
+document.getElementById('vinkel').classList.toggle('rotate'),
+document.getElementById('rullegardin-id').classList.toggle('rullegardin-show');
 });
 
-
-document.body.addEventListener('click', () => {
- if (dropdownContent.className === 'dropdown-content show-dropdown') {
-  dropdownContent.classList.toggle('show-dropdown');
-}
- });
